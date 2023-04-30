4e119e0efedb88ca8940c6534e67a74452798c73
it21038914 <it21038914@my.sliit.lk>
2023-05-01 00:14:18 +0530

    2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Book from "./pages/Book";
+import Navbar from "./components/Navbar";
+import BookList from "./pages/BookList";
+import NewBook from "./pages/NewBook";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books" element={<Book />} />
-    </Routes>
+    <>
+      <Navbar> </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
